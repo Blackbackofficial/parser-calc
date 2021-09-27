7ab6c6c3b0a1b18ae8d75eb3a59e4da05801a140
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-09-27 03:27:06 +0300

    Add func combineResults && construction

--- signer/signer.go
+++ signer/signer.go
@@ -1,3 +1,33 @@
 package main
 
-// сюда писать код
+import (
+	"sort"
+	"strings"
+)
+
+// ExecutePipeline which provides us with pipelining of worker functions that do something
+func ExecutePipeline(jobs ...job) {
+
+}
+
+// SingleHash considers the value crc32 (data) + "~" + crc32 (md5 (data)) (concatenation of two strings through ~), where data is what came to the input (in fact, numbers from the first function)
+func SingleHash(stdIn, out chan interface{}) {
+
+}
+
+// MultiHash counts the value crc32 (th + data)) (concatenation of a digit reduced to a string and a string), where th = 0..5, then takes the concatenation of the results in the order of calculation (0..5), where data is what came to the input
+func MultiHash(stdIn, out chan interface{}) {
+
+}
+
+
+// CombineResults gets all results, sorts (https://golang.org/pkg/sort/), concatenates the sorted result with _ (underscore) into one line
+func CombineResults(stdIn, stdOut chan interface{}) {
+	var concat []string
+	for v := range stdIn {
+		concat = append(concat, v.(string))
+	}
+
+	sort.Strings(concat)
+	stdOut <- strings.Join(concat, "_")
+}
