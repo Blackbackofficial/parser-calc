c30aaf3592a2e6a8e9968121633edcddad24f650
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-09-27 20:35:13 +0300

    Add SingleHash, MultiHash; deleted defer

--- signer/signer.go
+++ signer/signer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -14,12 +15,11 @@ func ExecutePipeline(jobs ...job) {
 	wg := &sync.WaitGroup{}
 	for _, someJob := range jobs {
 		wg.Add(1)
-		go func(wg *sync.WaitGroup, in, out chan interface{}, j job) {
-			// TODO: mb defer??
-			close(out)
+		go func(stdIn, stdOut chan interface{}, j job) {
+			j(stdIn, stdOut)
+			close(stdOut)
 			wg.Done()
-			j(in, out)
-		}(wg, stdIn, stdOut, someJob)
+		}(stdIn, stdOut, someJob)
 		stdIn = stdOut
 		stdOut = make(chan interface{})
 	}
@@ -27,15 +27,56 @@ func ExecutePipeline(jobs ...job) {
 }
 
 // SingleHash considers the value crc32 (data) + "~" + crc32 (md5 (data)) (concatenation of two strings through ~), where data is what came to the input (in fact, numbers from the first function)
-func SingleHash(stdIn, out chan interface{}) {
+func SingleHash(stdIn chan interface{}, stdOut chan interface{}) {
+	wg := &sync.WaitGroup{}
+	for input := range stdIn {
+		wg.Add(1)
+		strMd5 := DataSignerMd5(strconv.Itoa(input.(int)))
 
+		go func(str string) {
+			wg1 := &sync.WaitGroup{}
+			wg1.Add(2) // 2 process
+			var str1, str2 string
+
+			go func(data string) {
+				str1 = DataSignerCrc32(data)
+				wg1.Done()
+			}(str)
+
+			go func(data string) {
+				str2 = DataSignerCrc32(strMd5)
+				wg1.Done()
+			}(str)
+			wg1.Wait()
+			wg.Done()
+			stdOut <- strings.Join([]string{str1, str2}, "~")
+		}(strconv.Itoa(input.(int)))
+	}
+	wg.Wait()
 }
 
 // MultiHash counts the value crc32 (th + data), where th = 0..5, then takes the concatenation of the results in the order of calculation (0..5), where data is what came to the input
-func MultiHash(stdIn, out chan interface{}) {
-
+func MultiHash(stdIn chan interface{}, stdOut chan interface{}) {
+	wg := &sync.WaitGroup{}
+	for d := range stdIn {
+		wg.Add(1)
+		go func(stdOut chan interface{}, str string) {
+			strSigner := make([]string, 6)
+			wg1 := &sync.WaitGroup{}
+			wg1.Add(6) // 6 process: th = 0...5
+			for v := 0; v <= 5; v++ {
+				go func(res *string, data string, i string) {
+					*res = DataSignerCrc32(i + data)
+					wg1.Done()
+				}(&strSigner[v], str, strconv.Itoa(v))
+			}
+			wg1.Wait()
+			stdOut <- strings.Join(strSigner, "")
+			wg.Done()
+		}(stdOut, d.(string))
+	}
+	wg.Wait()
 }
-
 
 // CombineResults gets all results, sorts (https://golang.org/pkg/sort/), concatenates the sorted result with _ (underscore) into one line
 func CombineResults(stdIn, stdOut chan interface{}) {
