dc8bccbab993a7aff6faf5867c154c0b4a6db8f0
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-09-28 18:39:08 +0300

    Refactoring

--- signer/signer.go
+++ signer/signer.go
@@ -7,13 +7,15 @@ import (
 	"sync"
 )
 
+var th int = 6 // 6 process: th = 0...5
+
 // ExecutePipeline which provides us with pipelining of worker functions that do something
 func ExecutePipeline(jobs ...job) {
 	stdIn := make(chan interface{})
-	stdOut := make(chan interface{})
 
 	wg := &sync.WaitGroup{}
 	for _, someJob := range jobs {
+		stdOut := make(chan interface{})
 		wg.Add(1)
 		go func(stdIn, stdOut chan interface{}, j job) {
 			j(stdIn, stdOut)
@@ -21,7 +23,6 @@ func ExecutePipeline(jobs ...job) {
 			wg.Done()
 		}(stdIn, stdOut, someJob)
 		stdIn = stdOut
-		stdOut = make(chan interface{})
 	}
 	wg.Wait()
 }
@@ -37,16 +38,8 @@ func SingleHash(stdIn chan interface{}, stdOut chan interface{}) {
 			wg1 := &sync.WaitGroup{}
 			wg1.Add(2) // 2 process
 			var str1, str2 string
-
-			go func(data string) {
-				str1 = DataSignerCrc32(data)
-				wg1.Done()
-			}(str)
-
-			go func(data string) {
-				str2 = DataSignerCrc32(strMd5)
-				wg1.Done()
-			}(str)
+			go dataSignerCrc32(wg1, &str, &str1)
+			go dataSignerCrc32(wg1, &strMd5, &str2)
 			wg1.Wait()
 			defer wg.Done()
 			stdOut <- strings.Join([]string{str1, str2}, "~")
@@ -55,20 +48,23 @@ func SingleHash(stdIn chan interface{}, stdOut chan interface{}) {
 	wg.Wait()
 }
 
+func dataSignerCrc32(wg1 *sync.WaitGroup, str, out *string) {
+	*out = DataSignerCrc32(*str)
+	wg1.Done()
+}
+
 // MultiHash counts the value crc32 (th + data), where th = 0..5, then takes the concatenation of the results in the order of calculation (0..5), where data is what came to the input
 func MultiHash(stdIn chan interface{}, stdOut chan interface{}) {
 	wg := &sync.WaitGroup{}
 	for d := range stdIn {
 		wg.Add(1)
 		go func(stdOut chan interface{}, str string) {
-			strSigner := make([]string, 6)
+			strSigner := make([]string, th)
 			wg1 := &sync.WaitGroup{}
-			wg1.Add(6) // 6 process: th = 0...5
-			for v := 0; v <= 5; v++ {
-				go func(res *string, data string, i string) {
-					*res = DataSignerCrc32(i + data)
-					wg1.Done()
-				}(&strSigner[v], str, strconv.Itoa(v))
+			wg1.Add(th)
+			for v := 0; v < th; v++ {
+				str1 := strconv.Itoa(v)+str
+				go dataSignerCrc32(wg1, &str1, &strSigner[v])
 			}
 			wg1.Wait()
 			stdOut <- strings.Join(strSigner, "")
